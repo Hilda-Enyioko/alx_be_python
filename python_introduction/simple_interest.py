f5e99a3097c07b31ed082bd416e1e89dbe110d41
Hilda Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2025-09-10 14:55:42 +0000

    Task 2 completed

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+# Script Objective: Apply basic Python arithmetic operations and variable assignments to calculate the simple interest on a given principal amount, rate of interest, and time.
+#!/usr/bin/python3
+
+principal = 1000
+rate = 0.05
+time = 3
+
+interest = principal * rate * time
+
+print(f"The simple interest is: {interest}")
