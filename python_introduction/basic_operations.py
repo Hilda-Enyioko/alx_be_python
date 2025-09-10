81214b4026b4a5a01225984006bdae48a00dc090
Hilda Enyioko <105448377+Hilda-Enyioko@users.noreply.github.com>
2025-09-10 14:26:23 +0000

    removed keyword sum

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -3,12 +3,12 @@
 number1 = 10
 number2 =  5
 
-sum = number1 + number2
+addition = number1 + number2
 difference  = number1 - number2
 product = number1 * number2
 division = number1 / number2
 
-print(f"Addition of {number1} and {number2} is {sum}")
+print(f"Addition of {number1} and {number2} is {addition}")
 print(f"Subtraction of {number1} and {number2} is {difference}")
 print(f"Multiplication of {number1} and {number2} is {product}")
 print(f"Division of {number1} and {number2} is {division}")
