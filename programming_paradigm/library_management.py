d2634c7c29af375cc99dea7f9dfbcc4a02676e6b
Hilda Enyioko <enyiokohilda@gmail.com>
2025-10-05 13:08:12 +0100

    add check_out and return_book methodst to the book class

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -5,53 +5,62 @@ class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
-        # add a private attribute _is_checked_out initialized to False
+        # Private attribute to track checkout status
         self._is_checked_out = False
     
     def __str__(self):
         return f"'{self.title}' by {self.author}"
-    
+
+    # Method to check out the book
+    def check_out(self):
+        if not self._is_checked_out:
+            self._is_checked_out = True
+            return True
+        return False
+
+    # Method to return the book
+    def return_book(self):
+        if self._is_checked_out:
+            self._is_checked_out = False
+            return True
+        return False
+
+
 class Library:
-    #  a private list _books to store instances of Book.
     def __init__(self):
+        # Private list to store book instances
         self._books = []
     
-    # method to add a book to the library    
+    # Add a book to the library
     def add_book(self, book):
         self._books.append(book)
 
-    # method to remove a book from the library
+    # Remove a book from the library
     def remove_book(self, book):
         self._books.remove(book)
 
-    # method to find a book by its title
+    # Find a book by its title
     def find_book(self, title):
         for book in self._books:
             if book.title == title:
                 return book
         return None
 
-    # method to list all books in the library
-    def list_books(self):
-        return self._books
-
-    # method to check out a book (set _is_checked_out to True)
+    # Check out a book using the Book class method
     def check_out_book(self, title):
         book = self.find_book(title)
-        if book and not book._is_checked_out:
-            book._is_checked_out = True
-            return True
+        if book:
+            return book.check_out()
         return False
     
-    # method to return a book (set _is_checked_out to False)
+    # Return a book using the Book class method
     def return_book(self, title):
         book = self.find_book(title)
-        if book and book._is_checked_out:
-            book._is_checked_out = False
-            return True
+        if book:
+            return book.return_book()
         return False
-    
-    # method to list all available books (not checked out)
+
+    # List all available books (not checked out)
     def list_available_books(self):
         for book in self._books:
             if not book._is_checked_out:
